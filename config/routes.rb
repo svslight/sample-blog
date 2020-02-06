252fb86281196df60f6dd972e7236a4b23c308d4
Svetlana Solodovnikova <svslight@yandex.ru>
2020-02-06 18:28:52 +0500

    Change /contacts/new ->  /contacts

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
 
   # Описываем те методы, 
   # которые будут использоваться на странице контактов
-  resource :contacts, only: [:new, :create]
+  # Change /contacts/new  on  /contacts
+  # get 'contacts' => 'contacts#new'
+
+  resource :contacts, only: [:new, :create], path_names: { :new => '' }
   
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
