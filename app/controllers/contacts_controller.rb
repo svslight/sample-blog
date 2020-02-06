30e9bf4292154ff538a6a33b168c7aa12d075fc4
Svetlana Solodovnikova <svslight@yandex.ru>
2020-02-06 15:59:50 +0500

    Add contacts controller and new.html.erb view

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,12 @@
+class ContactsController < ApplicationController
+
+  # получение страницы
+  def new
+  end
+
+  # прием параметров на сервере и
+  # создание сущности в БД
+  def create
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get '/' => 'home#index'
 
+  # Описываем те методы, 
+  # которые будут использоваться на странице контактов
+  resource :contacts, only: [:new, :create]
+  
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
