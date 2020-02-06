5173da649cc17adbaef7fc67c2d4b64e7116a793
Svetlana Solodovnikova <svslight@yandex.ru>
2020-02-06 17:13:32 +0500

    Save contact on submit

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,9 +4,25 @@ class ContactsController < ApplicationController
   def new
   end
 
-  # прием параметров на сервере и
-  # создание сущности в БД
+  # прием параметров на сервере и создание сущности в БД
   def create
+    # как в Sinatr'e 
+    # @contact = Contact.new(params[:contact])
+    # в Ruby будет ош. использ запрещенных атрибутов
+    # нужно использовать метод private - нет доступа к методам из вне
+
+    # получение параметра из БД
+    @contact = Contact.new(contact_params)
+    # сохранение полученных данных в БД
+    @contact.save
+
+
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:email, :message)
   end
 
 end
