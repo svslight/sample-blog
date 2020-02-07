67da10155ec322508c0aa5e4e5ee1ad8f3fcfd92
Svetlana Solodovnikova <svslight@yandex.ru>
2020-02-07 12:23:00 +0500

    Implement articles#create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,28 @@
 class ArticlesController < ApplicationController
 
+  # возвращает в Браузер форму из new.html.erb
   def new    
   end
 
   def create
-    render plain: params[:article].inspect
+    # выводит содержимое на экран
+    # render plain: params[:article].inspect
+
+    # получение параметра из БД
+    @article = Article.new(article_params)
+
+    # сохранение полученных данных в БД 
+    if @article.valid?
+      @article.save
+    else
+      render action: 'new'
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 
 end
