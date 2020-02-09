9482e1766a9f6686f377b6f448a3fa904d1cfa93
Svetlana Solodovnikova <svslight@yandex.ru>
2020-02-09 17:04:16 +0500

    Implement acrticles#delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -42,6 +42,15 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
+
+
   private
 
   def article_params
